c06d6a39e639918825c38dc02f6d19ad138dfb38
Gaurav Srivastava <gauravjss@gmail.com>
2018-03-11 23:55:12 -0500

    Initial Commit

---
+++ namespaces/app.ts
@@ -0,0 +1,6 @@
+/// <reference path='circleMath.ts'/>
+/// <reference path='rectangleMath.ts'/>
+//const PI = 2.99;
+//console.log(PI);
+console.log(MyMath.calculateRectangle(10,20));
+console.log(MyMath.calculateCircumference(20));
