455b1e4b673516692e8d772a77aa6eb546d58f86
Gaurav Srivastava <gauravjss@gmail.com>
2018-03-12 09:02:57 -0500

    Adding Interfaces

---
+++ interfaces/interfaces.ts
@@ -0,0 +1,66 @@
+interface NamedPerson{
+    firstName: string;
+    //Optional
+    age?: number;
+    //Unknown Property Name, This is also optional
+    [prop:string] : any // Type can be number, string or anything else.
+    greet(lastName:string):void;
+}
+
+//Creating Objects from Interface
+const person: NamedPerson = {
+    firstName:'Gaurav',
+    gadgets:['iPhone','MacBook','Hp PC','iPad'],
+    greet(lastName: string){
+       console.log('Hi I am  '+ this.firstName+' '+lastName);
+    }
+};
+
+function changeName(person:NamedPerson){
+    person.firstName ='KD';
+}
+
+//Call
+changeName(person);
+person.greet('Srivastava');
+
+class Person implements NamedPerson{
+    firstName: string = 'Tech';
+    lastName: string = 'Geek';
+    greet(lastName: string){
+        console.log('Hi I am  '+ this.firstName+' '+lastName);
+    };
+}
+
+const myPerson = new Person();
+myPerson.firstName = 'Kds Changed';
+myPerson.greet('Srivastava');
+
+
+// Function Types
+interface DoubleValueFunc{
+    (number1:number, number2:number):number;
+}
+
+let myDoubleFunction: DoubleValueFunc;
+myDoubleFunction = function (value1:number, value2:number) {
+    return (value1 + value2)*2;
+};
+
+console.log(myDoubleFunction(10,20));
+
+
+// Interface Inheritence
+interface AgedPerson extends NamedPerson{
+    age:number;
+}
+
+const oldPerson:AgedPerson = {
+    age : 60,
+    firstName:'Max',
+        greet(lastName:string){
+        console.log('Hello');
+    }
+}
+
+oldPerson.greet('Subbu');
